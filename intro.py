4d0fedd908f3af69b09e77540b7b34974c68435c
Scholar <scholar@paradigm.local>
2023-08-29 13:11:23 -0600

    my first push

---
+++ intro.py
@@ -0,0 +1,10 @@
+#greeting = input("Hello, possible pirate! What's the password?")
+
+#if greeting in ("Arrr!"):
+
+	#print("Go away, pirate.")
+	
+#elif 0<5: print("Go away, pirate.") 
+#elif 0<3: print("true, but block will not execute") 
+#else: print("if all else fails")
+#print("Greetings, hater of pirates!")
