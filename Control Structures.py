2c90ab5f92c946079b3ec902d2e64b9953f6285b
Scholar <scholar@paradigm.local>
2023-09-19 13:32:20 -0600

    control structures

---
+++ Control Structures.py
@@ -0,0 +1,28 @@
+# car_color = "Blue"
+# car_make = "Tesla"
+
+# if car_color == "Blue" or car_color == "blue":
+#     if car_make == "Tesla":
+#          print("My car is a Blue Tesla")
+#     else:
+#          print("My car is blue")
+# elif car_color == "Red" or car_color == "red":
+#      print("My car is a ferrari")
+# else:
+#      print("I just have a bike")
+
+# for loop
+
+
+
+# sports = ['soccer', 'basketball', 'hockey, football']
+# print(sports)
+
+# for item in range(10):
+#     print(item)
+
+# While Loop
+# i = 1
+# while i < 6:
+#     print("I'm still going")
+#     i += 1
