ba9b2d0ab99ec3807a8dc8684f27c326ad150c57
Scholar <scholar@paradigm.local>
2023-09-28 13:23:39 -0600

    If statments

--- If statements.py
+++ If statements.py
@@ -1,20 +1,66 @@
 #Salary and bonus
 
-salary = int(input("What is your salary? "))
-years = int(input("How many years have you been working here? "))
+# salary = int(input("What is your salary? "))
+# years = int(input("How many years have you been working here? "))
 
-if years >= 5:
-    print(f"Congradulations, you get a bonus of ${salary * 0.05}.")
-else:
-    print("Sorry you do not qualify for a bonus.")
+# if years >= 5:
+#     print(f"Congradulations, you get a bonus of ${salary * 0.05}.")
+# else:
+#     print("Sorry you do not qualify for a bonus.")
 
 
 #Rectangle or square?
 
-First = int(input("What is the length of the rectangle? "))
-Second = int(input("what is the width of the rectangle? "))
+# First = int(input("What is the length of the rectangle? "))
+# Second = int(input("what is the width of the rectangle? "))
 
-if First == Second:
-    print("This rectangle is actually a square!")
+# if First == Second:
+#     print("This rectangle is actually a square!")
+# else:
+#     print("This is just a rectangle")
+
+#What Number is bigger?
+
+# FirstValue = int(input("What is the first value? " ))
+# SecondValue = int(input("What is the second value?" ))
+
+# if FirstValue > SecondValue:
+#     print(f"Your first value, {FirstValue}, is bigger than {SecondValue}" )
+# else:
+#     print(f"Your second value, {SecondValue}, is bigger than {FirstValue}" )
+# if FirstValue == SecondValue:
+#     print(f"Your first value, {FirstValue}, and {SecondValue} are the same... why did you need help exactly?" )
+# Who is oldest of three people?
+
+# Age1 = int(input("What is the first age? "))
+# Age2 = int(input("Great, What is the second age? "))
+# Age3 = int(input("Awesome, What is the Last age? "))
+
+# if Age1 > Age2 > Age3:
+#     print("The first age is the biggest, the second age is the next biggest, and the third age is the smallest." )
+# if Age1 > Age3 > Age2:
+#     print("The first age is the biggest, the third age is the next biggest, and the second age is the smallest." )
+# if Age2 > Age1 > Age3:
+#     print("The Second age is the biggest, the first age is the next biggest, and the third age is the smallest." )
+# if Age2 > Age3 > Age1:
+#     print("The Second age is the biggest, the third age is the next biggest, and the first age is the smallest." )
+# if Age3 > Age1 > Age2:
+#     print("The third age is the biggest, the first age is the next biggest, and the second age is the smallest." )
+# if Age3 > Age2 > Age1:
+#     print("The third age is the biggest, the second age is the next biggest, and the first age is the smallest." )
+# if Age1 == Age2 == Age3:
+#     print("All three ages are the same... why did you need help exactly?")
+
+# Are you allowed to take the exam?
+
+ClassesHeld = int(input("So, how many classes have been held? "))
+ClassesAttended = int(input("And... how many of those classes have you actually gone to? "))
+
+
+if (ClassesAttended / ClassesHeld) >= 0.75:
+    print(f"Your attendance is {(ClassesAttended / ClassesHeld) * 100}%")
+    print("You may attend the exam, good luck...")
 else:
-    print("This is just rectangle")
+    print(f"Your attendance is {(ClassesAttended / ClassesHeld) * 100}%")
+    print("you may not attend. Your presence is no longer needed. Your falure bring dishonor to you and your school")
+    
