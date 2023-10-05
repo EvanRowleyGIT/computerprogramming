adb113a6b3ff3b6afa51e1605b9f439b892d449e
Scholar <scholar@paradigm.local>
2023-10-05 13:26:15 -0600

    rock paper scissors

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,64 @@
+PlayAgain = "again"
+while (PlayAgain == "again"):
+
+    player1 = input("Player one, do you choose Rock, Paper, or Scissors? ")
+
+    print()
+
+    player2 = input("Player two, do you choose Rock, Paper, or Scissors? ")
+
+    print()
+
+    if player1 == "Rock":
+        if player2 == "Rock":
+            print("The game is a draw, you both chose rock!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+    
+    if player1 == "Paper":
+        if player2 == "Paper":
+            print("The game is a draw, you both chose paper!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player1 == "Scissors":
+        if player2 == "Scissors":
+            print("The game is a draw, you both chose scissors!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player1 == "Rock":
+        if player2 == "Scissors":
+            print("Player one wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player1 == "Paper":
+        if player2 == "Rock":
+            print("Player one wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player1 == "Scissors":
+        if player2 == "Paper":
+            print("Player one wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player2 == "Rock":
+        if player1 == "Scissors":
+            print("Player two wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player2 == "Paper":
+        if player1 == "Rock":
+            print("Player two wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
+
+    if player2 == "Scissors":
+        if player1 == "Paper":
+            print("Player two wins!")
+
+            print("Do you want to play again? Type Ctrl+C+C to stop")
